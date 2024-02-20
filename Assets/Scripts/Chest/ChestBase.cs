6a2176b257b309680b728ca41a5b74d7839a5f77
JefersonSerafina <jeferson.serafina@gmail.com>
2024-02-19 21:03:15 -0300

    Exercício 36
    Baú e árvore adicionados, ambos dão moedas

--- Assets/Scripts/Chest/ChestBase.cs
+++ Assets/Scripts/Chest/ChestBase.cs
@@ -1,8 +1,91 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ChestBase : MonoBehaviour
 {
+    public KeyCode keyCode = KeyCode.Z;
+    public Animator animator;
+    public string triggerOpen = "Open";
+
+    [Header("Notification")]
+    public GameObject notification;
+    public float tweenDuration = .2f;
+    public Ease tweenEase = Ease.OutBack;
+
+    [Space]
+    public ChestItemBase chestItem;
     
+    private float startScale;
+
+    private bool _chestOpened = false;
+
+    private void Start()
+    {
+        startScale = notification.transform.localScale.x;
+        HideNotification();
+    }
+    private void OpenChest()
+    {
+        if (_chestOpened) return;
+
+        animator.SetTrigger(triggerOpen);
+        _chestOpened = true;
+        HideNotification();
+        chestItem.ShowItem();
+        Invoke(nameof(ShowItem), 1f);
+    }
+
+    private void ShowItem()
+    {
+        chestItem.ShowItem();
+        Invoke(nameof(CollectItem), 1f);
+    }
+
+    private void CollectItem()
+    {
+        chestItem.Collect();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player p = other.transform.GetComponent<Player>();
+        if(p != null)
+        {
+            ShowNotification();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Player p = other.transform.GetComponent<Player>();
+        if (p != null)
+        {
+            HideNotification();
+        }
+    }
+
+    [NaughtyAttributes.Button]
+    private void ShowNotification()
+    {
+        notification.SetActive(true);
+        notification.transform.localScale = Vector3.zero;
+        notification.transform.DOScale(startScale, tweenDuration);
+    }
+
+    [NaughtyAttributes.Button]
+    private void HideNotification()
+    {
+        notification.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(keyCode) && notification.activeSelf)
+        {
+            OpenChest();
+        }
+    }
+
 }
---
+++ Assets/Scripts/Items/DestructableItem/DestructableItemBase.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DestructableItemBase : MonoBehaviour
+{
+    public HealthBase healthBase;
+
+    public int dropCoinsAmount = 10;
+    public GameObject coinPrefab;
+    public Transform dropPosition;
+
+    private void OnValidate()
+    {
+        if(healthBase == null) healthBase = GetComponent<HealthBase>();
+    }
+
+    private void Awake()
+    {
+        OnValidate();
+        healthBase.OnDamage += OnDamage;
+    }
+
+    private void OnDamage(HealthBase h)
+    {
+        transform.DOShakeScale(.1f, Vector3.up, 5);
+        DropCoins();
+    }
+
+    private void DropCoins()
+    {
+        var i = Instantiate(coinPrefab);
+        i.transform.position = dropPosition.position;
+        i.transform.DOScale(0, 1f).SetEase(Ease.OutBack).From();
+    }
+
+}
